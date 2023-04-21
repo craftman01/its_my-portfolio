e31cf9fb25e5a53b43e7d49f00bd4cb6f7209e3a
craftman01 <mrsuraj42@gmail.com>
2023-04-21 19:35:36 +0530

    added demo work

--- src/component/Portfolio.jsx
+++ src/component/Portfolio.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import Partfolio from "../assets/images.jpeg"
+import BRAHMA from "../assets/Brahma23.png"
+import HACK from "../assets/Hackathon.png"
 
 const Portfolio = () => {
 
@@ -13,29 +15,29 @@ const Portfolio = () => {
                         Portfolio
                     </p>
                     <p className='py-6' >
-                        Check my Works
+                        Check my Works 
                     </p>
                 </div>
                 <div className='grid sm:grid-cols-2 md:grid-cols-3 gap-8 px-12 sm:px-0 ' >
                     <div className=' shadow-md shadow-gray-600  rounded-lg  ' >
-                        <img src={Partfolio} alt=' ' className='  rounded-md duration-200 hover:scale-105 w-full ' />
+                        <img src={BRAHMA} alt='brahma' className='  rounded-md duration-200 hover:scale-105 w-full ' />
                         <div className='flex items-center justify-between  '>
-                            <button className='w-1/2 px-6 py-3 duration-200 hover:scale-105  '  >
-                                Demo
+                            <button className=' w-1/2 font-bold px-6 py-3 duration-200 hover:scale-105  '  >
+                                <a href='https://brahma23.live/' >Demo</a>
                             </button >
-                            <button className='w-1/2 px-6 py-3 duration-200 hover:scale-105  '  >
-                                Code
+                            <button className='w-1/2 px-6 py-3 font-bold duration-200 hover:scale-105  '  >
+                                <a href='https://github.com/r0king/brahma.git'>Code</a>
                             </button >
                         </div>
                     </div>
                     <div className=' shadow-md shadow-gray-600  rounded-lg  ' >
-                        <img src={Partfolio} alt=' ' className='  rounded-md duration-200 hover:scale-105 w-full ' />
+                        <img src={HACK} alt='Hackathon ' className='  rounded-md duration-200 hover:scale-105 w-full ' />
                         <div className='flex items-center justify-between  '>
-                            <button className='w-1/2 px-6 py-3 duration-200 hover:scale-105  '  >
-                                Demo
+                            <button className='w-1/2 px-6 py-3  font-bold duration-200 hover:scale-105  '  >
+                              <a href='https://hackathon.adishankara.ac.in/' >Demo</a>  
                             </button >
-                            <button className='w-1/2 px-6 py-3 duration-200 hover:scale-105  '  >
-                                Code
+                            <button className='w-1/2 px-6 py-3 font-bold duration-200 hover:scale-105  '  >
+                                <a href='https://github.com/r0king/asiet-hackathon.git' >Code</a>
                             </button >
                         </div>
                     </div>
