2278c1f011cc56274256daf137526245ee6ae4ff
craftman01 <mrsuraj42@gmail.com>
2023-06-04 16:43:32 +0530

    my cv

--- src/component/Social.jsx
+++ src/component/Social.jsx
@@ -55,7 +55,7 @@ const Social = () => {
                 <li  
                  className={ "flex justify-between items-center w-40 h-14 px-4 ml-[-100px] hover:ml-[-10px] hover:rounded-md duration-300  bg-black"  }>
                     <a 
-                       href='/Craftman.pdf'
+                       href='Craftman.pdf'
                        download="craftman"
                        className='flex justify-between items-center w-full text-white ' 
                           
